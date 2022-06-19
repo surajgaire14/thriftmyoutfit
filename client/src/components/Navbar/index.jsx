d91cefcc533a672084457bb251bd501ccd2a3eda
theboringschool <theboringschool.org@gmail.com>
2022-06-19 12:16:51 +0545

    Added Navbar Large

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,39 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import './styles/style.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar__container'>
+        <div className="left">
+            <ul>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+                <li>Lorem</li>
+            </ul>
+        </div>
+        <div className="center">
+            Thrift My Outfit
+        </div>
+        <div className="right">
+            <ul>
+                <li>
+                    <form>
+                    <i className="ri-search-line"></i>
+                        <input type="text" placeholder="Search"/>
+                    </form>
+                </li>
+                <li>
+                    <Button>
+                    <i className="ri-user-line"></i>
+                    </Button>
+                </li>
+                <li>
+                    <Button><i className="ri-shopping-bag-3-line"></i></Button>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
